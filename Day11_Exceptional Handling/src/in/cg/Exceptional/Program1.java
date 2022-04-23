1f05928441521ee73fd2e41cf2f346ba527b1ba8
rr <rr@DESKTOP-29GHDV2>
2022-04-23 20:49:54 +0530

    First Commit

---
+++ Day11_Exceptional Handling/src/in/cg/Exceptional/Program1.java
@@ -0,0 +1,30 @@
+package in.cg.Exceptional;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+		int x=12/0;
+		System.out.println(x);
+		}
+		catch(Exception e)
+		{
+		
+		System.out.println("Exception Handled " +e);
+
+	}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
